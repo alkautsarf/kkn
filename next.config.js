5ff335616b4bbef073970d9d2fdf0a3ae401ba58
Tama <arifnovrian@gmail.com>
2023-07-12 23:00:26 +0700

    testing

--- next.config.js
+++ next.config.js
@@ -6,3 +6,28 @@ const nextConfig = {
 };
 
 module.exports = withPlausibleProxy()(nextConfig);
+
+const withVideos = require('next-videos')
+
+module.exports = withVideos()
+
+// headers: async () => [
+//   {
+//     source: "/api/:path*",
+//     headers: [
+//       { key: "Access-Control-Allow-Origin", value: "*" },
+//       { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//       { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, Accept" },
+//     ],
+//   },
+// ]
+
+// const withVideos = require('next-videos')
+
+// module.exports = withVideos({
+//   // assetPrefix: 'https://d1s8hc4jzg476k.cloudfront.net/video2.mp4',
+
+//   webpack(config, options) {
+//     return config
+//   }
+// })
